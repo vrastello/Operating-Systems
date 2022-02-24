78affd307d191b750f9bcc20c1fbeab057c769d2
Vincent Rastello <vrastello11@gmail.com>
2022-02-23 21:31:50 -0800

    adding more logic

--- A4/line_processor.c
+++ A4/line_processor.c
@@ -12,29 +12,29 @@ A program with a pipeline of 3 threads that interact with each other as producer
 - Output thread is the third thread in the pipeline. It consumes items from the buffer it shares with the square root thread and prints the items.
 */
 
-#define L_MAX 50
-#define C_MAX 1000
-#define NUM_L = 49
+#define LINE_MAX 50
+#define CHAR_MAX 1000
+#define OUT_MAX 80
 #define TERMINATOR "STOP\n"
 
-char buffer_1[L_MAX][C_MAX];
+char buffer_1[LINE_MAX][CHAR_MAX];
 int count_1 = 0;
 int prod_idx_1 = 0;
 int con_idx_1 = 0;
 
-char buffer_2[L_MAX][C_MAX];
+char buffer_2[LINE_MAX][CHAR_MAX];
 int count_2 = 0;
 int prod_idx_2 = 0;
 int con_idx_2 = 0;
 
-char buffer_3[L_MAX][C_MAX];
+char buffer_3[LINE_MAX * CHAR_MAX * sizeof(char)];
 int count_3 = 0;
 int prod_idx_3 = 0;
 int con_idx_3 = 0;
 
 char *get_user_input(){
-  char *user_input = malloc(C_MAX * sizeof(char));
-  fgets(user_input, C_MAX, stdin);
+  char *user_input = malloc(CHAR_MAX * sizeof(char));
+  fgets(user_input, CHAR_MAX, stdin);
   return user_input;
 }
 
@@ -51,22 +51,24 @@ void put_buff_1(char *input){
 
 void *get_input()
 {
-  char input[C_MAX];
+  char input[CHAR_MAX];
   while(strncmp(input, TERMINATOR, strlen(TERMINATOR)) != 0){
     strcpy(input, get_user_input());
     if(strncmp(input, TERMINATOR, strlen(input)) == 0){
       break;
     }
     put_buff_1(input);
+    memset(input, '\0', CHAR_MAX);
   }
   return NULL;
 }
 
 char *get_buff_1(){
-  char input[] = buffer_1[con_idx_1];
+  char *buff_1_input = malloc(CHAR_MAX * sizeof(char));
+  strcpy(buff_1_input, buffer_1[con_idx_1]);
   con_idx_1 = con_idx_1 + 1;
   count_1--;
-  return input;
+  return buff_1_input;
 }
 
 void put_buff_2(char *input){
@@ -77,17 +79,94 @@ void put_buff_2(char *input){
   count_2++;
 }
 
+char* expand_input(char* input, char* target, char* replacement) {
+    //parts of this code were inspired from my code from project 3
+
+    //find first instance of target, sets pointer to that part of string
+    char* input_pointer = strstr(input, target);
+    //if none found return null breaks loop
+    if (input_pointer == NULL) {
+        return NULL;
+    }
+
+    //inserts additonal memory into string at substring_source pointer to expand for replacement
+    memmove(
+        input_pointer + strlen(replacement),
+        input_pointer + strlen(target),
+        strlen(input_pointer) - strlen(target) + 1
+    );
+
+    //copies replacement starting at pointer where first instance of target starts
+    memcpy(input_pointer, replacement, strlen(replacement));
+
+    // returns pointer to where replacement string ends to run code on the rest of the string
+    return input_pointer + strlen(replacement);
+}
+
 void *line_separator()
 {
+  char input[CHAR_MAX];
+  memset(input, '\0', CHAR_MAX);
+  while(strncmp(input, TERMINATOR, strlen(TERMINATOR)) != 0){
+      strcpy(input, get_buff_1());
+      if(strncmp(input, TERMINATOR, strlen(input)) == 0){
+        break;
+      }
+      expand_input(input, "\n", " ");
+      put_buff_2(input);
+      memset(input, '\0', CHAR_MAX);
+  }
   
+  return NULL;
+}
+
+char *get_buff_2(){
+  char *buff_2_input = malloc(CHAR_MAX * sizeof(char));
+  strcpy(buff_2_input, buffer_2[con_idx_2]);
+  con_idx_2 = con_idx_2 + 1;
+  count_2--;
+  return buff_2_input;
+}
+
+void put_buff_3(char *input){
+  // Put the item in the buffer
+  int len = strlen(input);
+  strcpy(&buffer_3[prod_idx_3], input);
+  // Increment the index where the next item will be put.
+  prod_idx_3 = prod_idx_3 + len;
+  count_3++;
+}
+
+void *plus_sign()
+{
+  char input[CHAR_MAX];
+  memset(input, '\0', CHAR_MAX);
+  while(strncmp(input, TERMINATOR, strlen(TERMINATOR)) != 0){
+      strcpy(input, get_buff_2());
+      if(strncmp(input, TERMINATOR, strlen(input)) == 0){
+        break;
+      }
+      while(expand_input(input, "++", "^"));
+      put_buff_3(input);
+      memset(input, '\0', CHAR_MAX);
+  }
+  
+  return NULL;
+}
+
+void *write_output()
+{
+  char output[OUT_MAX + 1];
 }
 
 int main()
 {
   get_input();
-  for(int i = 0; i < L_MAX; i++){
-    printf("%s", buffer_1[i]);
-  }
+  line_separator();
+  plus_sign();
+  printf("%s", buffer_3);
+  printf("%d", strlen(buffer_3));
+  
   return EXIT_SUCCESS;
 }
 
