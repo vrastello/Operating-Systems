2bfde7845304d3a476707e16ff7218aa5a927ec2
Vincent Rastello <vrastello11@gmail.com>
2022-02-24 15:26:11 -0800

    finished up comments

--- A4/line_processor.c
+++ A4/line_processor.c
@@ -1,3 +1,7 @@
+/* 
+Sources: all explorations from CS 344, and experience from previous projects!
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,12 +9,13 @@
 #include <unistd.h>
 #include <math.h> 
 
-
+// define all constants 
 #define LINE_MAX 50
 #define CHAR_MAX 1000
 #define OUT_MAX 80
 #define TERMINATOR "STOP\n"
 
+// set all buffers, consume and produce indexes and thread blockers and conditionals
 char buffer_1[LINE_MAX][CHAR_MAX];
 int count_1 = 0;
 int prod_idx_1 = 0;
@@ -38,6 +43,7 @@ pthread_mutex_t mutex_3 = PTHREAD_MUTEX_INITIALIZER;
 // Initialize the condition variable for buffer 2
 pthread_cond_t full_3 = PTHREAD_COND_INITIALIZER;
 
+// collect user input from stdin
 char *get_user_input(){
   char *user_input = malloc(CHAR_MAX * sizeof(char));
   fgets(user_input, CHAR_MAX, stdin);
@@ -61,8 +67,10 @@ void put_buff_1(char *input){
   pthread_mutex_unlock(&mutex_1);
 }
 
+// get input from user
 void *get_input(void *args)
 {
+  // save user input then check for terminator, keep producing for buffer_1 until terminator
   char input[CHAR_MAX];
   memset(input, '\0', CHAR_MAX);
   strcpy(input, get_user_input());
@@ -72,6 +80,7 @@ void *get_input(void *args)
     strcpy(input, get_user_input());
   }
 
+  // add terminator to buffer_1 so last thread knows when to stop
   put_buff_1(input);
   return NULL;
 }
@@ -83,6 +92,7 @@ char *get_buff_1(){
     // Buffer is empty. Wait for the producer to signal that the buffer has data
     pthread_cond_wait(&full_1, &mutex_1);
   }
+  // put buffer_1 index into heap and increase index, decrease count 
   char *buff_1_input = malloc(CHAR_MAX * sizeof(char));
   strcpy(buff_1_input, buffer_1[con_idx_1]);
   con_idx_1 = con_idx_1 + 1;
@@ -106,6 +116,7 @@ void put_buff_2(char *input){
   pthread_mutex_unlock(&mutex_2);
 }
 
+// this can expand input for both ++ and " " to replace items
 char* expand_input(char* input, char* target, char* replacement) {
     //parts of this code were inspired from my code from project 3
 
@@ -130,17 +141,21 @@ char* expand_input(char* input, char* target, char* replacement) {
     return input_pointer + strlen(replacement);
 }
 
+// inserts " " where newline is
 void *line_separator(void *args)
 {
+  // get from buffer_1
   char input[CHAR_MAX];
   memset(input, '\0', CHAR_MAX);
   strcpy(input, get_buff_1());
+  //keep consuming from buffer one, expanding input and producing for buffer two until terminator
   while(strncmp(input, TERMINATOR, strlen(TERMINATOR)) != 0){
       expand_input(input, "\n", " ");
       put_buff_2(input);
       memset(input, '\0', CHAR_MAX);
       strcpy(input, get_buff_1());
   }
+  // save terminator to buffer 2
   put_buff_2(input);
   return NULL;
 }
@@ -152,6 +167,7 @@ char *get_buff_2(){
     // Buffer is empty. Wait for the producer to signal that the buffer has data
     pthread_cond_wait(&full_2, &mutex_2);
   }
+  // put buffer_2 item on heap and increase index, decrease count
   char *buff_2_input = malloc(CHAR_MAX * sizeof(char));
   strcpy(buff_2_input, buffer_2[con_idx_2]);
   con_idx_2 = con_idx_2 + 1;
@@ -175,17 +191,22 @@ void put_buff_3(char *input){
   pthread_mutex_unlock(&mutex_3);
 }
 
+// replaces ++ with ^
 void *plus_sign(void *args)
 {
+  //save buffer_2 item
   char input[CHAR_MAX];
   memset(input, '\0', CHAR_MAX);
   strcpy(input, get_buff_2());
+    //keep consuming from buffer two, expanding input and producing for buffer three until terminator
   while(strncmp(input, TERMINATOR, strlen(TERMINATOR)) != 0){
+      //loop through entire string to replace all instances
       while(expand_input(input, "++", "^"));
       put_buff_3(input);
       memset(input, '\0', CHAR_MAX);
       strcpy(input, get_buff_2());
   }
+  // put terminator in buffer 3 so output knows when to stop
   put_buff_3(input);
   return NULL;
 }
@@ -196,6 +217,7 @@ char *get_buff_3(){
   while (count_3 == 0)
     // Buffer is empty. Wait for the producer to signal that the buffer has data
     pthread_cond_wait(&full_3, &mutex_3);
+  // put buffer 3 item on heap, increase index, decrease count
   char *buff_3_input = malloc(CHAR_MAX * sizeof(char));
   strcpy(buff_3_input, buffer_3[con_idx_3]);
   con_idx_3 = con_idx_3 + 1;
@@ -205,6 +227,7 @@ char *get_buff_3(){
   return buff_3_input;
 }
 
+// function for strings larger than 80 that need to be printed and remainder saved back to buffer 3
 void process_buff_3(char *large_string)
 {
   //take input, separate out substring of 80, save remainder to current index, print substring
@@ -213,44 +236,54 @@ void process_buff_3(char *large_string)
   strcpy(&buffer_3[con_idx_3][0], large_string + OUT_MAX);
 }
 
+// writes output of only 80 length strings, adds up smaller strings or breaks up larger strings to accomplish this
 void *write_output(void *args)
 {
+  //get buffer 3 string
   char *input = malloc(CHAR_MAX * sizeof(char));
   memset(input, '\0', CHAR_MAX);
   strcpy(input, get_buff_3());
+  //until we get terminator...
   while(strncmp(input, TERMINATOR, strlen(TERMINATOR)) != 0){
-      // put code here LOL
+      // if length is exactly 80 just print, if greater use process_buff_3 function
       if(strlen(input) >= OUT_MAX){
         if(strlen(input) == OUT_MAX){
           printf("%s\n", input);
         }
         else{
+          // increase count and decrement index because we will be adding back remainder to buffer_3
           count_3++;
           con_idx_3 = con_idx_3 - 1;
           process_buff_3(input);
         }
       }
+      // if length is less than 80
       else{
+        // continue concatenating strings together until length is above 80
         char *curr_str = malloc(CHAR_MAX * 2 * sizeof(char));
         char next_str[CHAR_MAX];
         strcpy(curr_str, input);
         while(strlen(curr_str) < OUT_MAX){
           strcpy(next_str, get_buff_3());
+          // if terminator reached while length is still under 80, skip string and end program
           if (strncmp(next_str, TERMINATOR, strlen(TERMINATOR)) == 0){
             return NULL;
           }
           strcat(curr_str, next_str);
           memset(next_str, '\0', CHAR_MAX);
         }
+        // if concatenated string length is exactly 80 print
         if(strlen(curr_str) == OUT_MAX){
           printf("%s\n", input);
         }
         else{
+          // if concatenated string length greater than 80 process_buff_3 to print substring and save remainder back to buff_3
           count_3++;
           con_idx_3 = con_idx_3 - 1;
           process_buff_3(curr_str);
         }
       }
+      // get next item in buff 3
       memset(input, '\0', CHAR_MAX);
       strcpy(input, get_buff_3());
   }
