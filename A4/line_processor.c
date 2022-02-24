77ac67bb1dadee991954c3abfffc0b1fbb289e9f
Vincent Rastello <vrastello11@gmail.com>
2022-02-24 02:34:59 -0800

    adding threading

--- A4/line_processor.c
+++ A4/line_processor.c
@@ -3,14 +3,8 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <math.h> // must link with -lm
+#include <math.h> 
 
-/*
-A program with a pipeline of 3 threads that interact with each other as producers and consumers.
-- Input thread is the first thread in the pipeline. It gets input from the user and puts it in a buffer it shares with the next thread in the pipeline.
-- Square root thread is the second thread in the pipeline. It consumes items from the buffer it shares with the input thread. It computes the square root of this item. It puts the computed value in a buffer it shares with the next thread in the pipeline. Thus this thread implements both consumer and producer functionalities.
-- Output thread is the third thread in the pipeline. It consumes items from the buffer it shares with the square root thread and prints the items.
-*/
 
 #define LINE_MAX 50
 #define CHAR_MAX 1000
@@ -21,16 +15,28 @@ char buffer_1[LINE_MAX][CHAR_MAX];
 int count_1 = 0;
 int prod_idx_1 = 0;
 int con_idx_1 = 0;
+// Initialize the mutex for buffer 1
+pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;
+// Initialize the condition variable for buffer 1
+pthread_cond_t full_1 = PTHREAD_COND_INITIALIZER;
 
 char buffer_2[LINE_MAX][CHAR_MAX];
 int count_2 = 0;
 int prod_idx_2 = 0;
 int con_idx_2 = 0;
+// Initialize the mutex for buffer 2
+pthread_mutex_t mutex_2 = PTHREAD_MUTEX_INITIALIZER;
+// Initialize the condition variable for buffer 2
+pthread_cond_t full_2 = PTHREAD_COND_INITIALIZER;
 
 char buffer_3[LINE_MAX][CHAR_MAX];
 int count_3 = 0;
 int prod_idx_3 = 0;
 int con_idx_3 = 0;
+// Initialize the mutex for buffer 2
+pthread_mutex_t mutex_3 = PTHREAD_MUTEX_INITIALIZER;
+// Initialize the condition variable for buffer 2
+pthread_cond_t full_3 = PTHREAD_COND_INITIALIZER;
 
 char *get_user_input(){
   char *user_input = malloc(CHAR_MAX * sizeof(char));
@@ -42,14 +48,20 @@ char *get_user_input(){
  Put an item in buff_1
 */
 void put_buff_1(char *input){
+  // Lock the mutex before putting the item in the buffer
+  pthread_mutex_lock(&mutex_1);
   // Put the item in the buffer
   strcpy(&buffer_1[prod_idx_1][0], input);
   // Increment the index where the next item will be put.
   prod_idx_1 = prod_idx_1 + 1;
   count_1++;
+  // Signal to the consumer that the buffer is no longer empty
+  pthread_cond_signal(&full_1);
+  // Unlock the mutex
+  pthread_mutex_unlock(&mutex_1);
 }
 
-void *get_input()
+void *get_input(void *args)
 {
   char input[CHAR_MAX];
   memset(input, '\0', CHAR_MAX);
@@ -65,19 +77,33 @@ void *get_input()
 }
 
 char *get_buff_1(){
+  // Lock the mutex before checking if the buffer has data
+  pthread_mutex_lock(&mutex_1);
+  while(count_1 == 0){
+    // Buffer is empty. Wait for the producer to signal that the buffer has data
+    pthread_cond_wait(&full_1, &mutex_1);
+  }
   char *buff_1_input = malloc(CHAR_MAX * sizeof(char));
   strcpy(buff_1_input, buffer_1[con_idx_1]);
   con_idx_1 = con_idx_1 + 1;
   count_1--;
+  // Unlock the mutex
+  pthread_mutex_unlock(&mutex_1);
   return buff_1_input;
 }
 
 void put_buff_2(char *input){
+  // Lock the mutex before putting the item in the buffer
+  pthread_mutex_lock(&mutex_2);
   // Put the item in the buffer
   strcpy(&buffer_2[prod_idx_2][0], input);
   // Increment the index where the next item will be put.
   prod_idx_2 = prod_idx_2 + 1;
   count_2++;
+  // Signal to the consumer that the buffer is no longer empty
+  pthread_cond_signal(&full_2);
+  // Unlock the mutex
+  pthread_mutex_unlock(&mutex_2);
 }
 
 char* expand_input(char* input, char* target, char* replacement) {
@@ -104,7 +130,7 @@ char* expand_input(char* input, char* target, char* replacement) {
     return input_pointer + strlen(replacement);
 }
 
-void *line_separator()
+void *line_separator(void *args)
 {
   char input[CHAR_MAX];
   memset(input, '\0', CHAR_MAX);
@@ -120,22 +146,36 @@ void *line_separator()
 }
 
 char *get_buff_2(){
+  // Lock the mutex before checking if the buffer has data
+  pthread_mutex_lock(&mutex_2);
+  while(count_2 == 0){
+    // Buffer is empty. Wait for the producer to signal that the buffer has data
+    pthread_cond_wait(&full_2, &mutex_2);
+  }
   char *buff_2_input = malloc(CHAR_MAX * sizeof(char));
   strcpy(buff_2_input, buffer_2[con_idx_2]);
   con_idx_2 = con_idx_2 + 1;
   count_2--;
+  // Unlock the mutex
+  pthread_mutex_unlock(&mutex_2);
   return buff_2_input;
 }
 
 void put_buff_3(char *input){
+  // Lock the mutex before putting the item in the buffer
+  pthread_mutex_lock(&mutex_3);
   // Put the item in the buffer
   strcpy(&buffer_3[prod_idx_3][0], input);
   // Increment the index where the next item will be put.
   prod_idx_3 = prod_idx_3 + 1;
   count_3++;
+  // Signal to the consumer that the buffer is no longer empty
+  pthread_cond_signal(&full_3);
+  // Unlock the mutex
+  pthread_mutex_unlock(&mutex_3);
 }
 
-void *plus_sign()
+void *plus_sign(void *args)
 {
   char input[CHAR_MAX];
   memset(input, '\0', CHAR_MAX);
@@ -151,10 +191,17 @@ void *plus_sign()
 }
 
 char *get_buff_3(){
+  // Lock the mutex before checking if the buffer has data
+  pthread_mutex_lock(&mutex_3);
+  while (count_3 == 0)
+    // Buffer is empty. Wait for the producer to signal that the buffer has data
+    pthread_cond_wait(&full_3, &mutex_3);
   char *buff_3_input = malloc(CHAR_MAX * sizeof(char));
   strcpy(buff_3_input, buffer_3[con_idx_3]);
   con_idx_3 = con_idx_3 + 1;
   count_3--;
+  // Unlock the mutex
+  pthread_mutex_unlock(&mutex_3);
   return buff_3_input;
 }
 
@@ -166,7 +213,7 @@ void process_buff_3(char *large_string)
   strcpy(&buffer_3[con_idx_3][0], large_string + OUT_MAX);
 }
 
-void *write_output()
+void *write_output(void *args)
 {
   char *input = malloc(CHAR_MAX * sizeof(char));
   memset(input, '\0', CHAR_MAX);
@@ -212,10 +259,17 @@ void *write_output()
 
 int main()
 {
-  get_input();
-  line_separator();
-  plus_sign();
-  write_output();
+  pthread_t input_t, line_separator_t, plus_sign_t, output_t;
+  // Create the threads
+  pthread_create(&input_t, NULL, get_input, NULL);
+  pthread_create(&line_separator_t, NULL, line_separator, NULL);
+  pthread_create(&plus_sign_t, NULL, plus_sign, NULL);
+  pthread_create(&output_t, NULL, write_output, NULL);
+  // Wait for the threads to terminate
+  pthread_join(input_t, NULL);
+  pthread_join(line_separator_t, NULL);
+  pthread_join(plus_sign_t, NULL);
+  pthread_join(output_t, NULL);
 
   return EXIT_SUCCESS;
 }
